fee9d2a4f806d38a9ed357c6adf22774aa1af582
ralphgalan <ralph@acw.uk.com>
2018-03-09 14:46:56 +0000

    Clean up

--- assets/js/dependencies.js
+++ assets/js/dependencies.js
@@ -1,11 +1,17 @@
 // jQuery => http://api.jquery.com/
-window.jQuery = window.$ = require('jquery');
+// window.jQuery = window.$ = require('jquery');
 
 // ChartJS => http://www.chartjs.org
-window.chart = require('chart.js');
+// window.Chart = require('chart.js');
 
 // Lodash => https://lodash.com
-window._ = require('lodash');
+// window._ = require('lodash');
 
 // Matter.js => http://brm.io/matter-js/docs/index.html
 window.Matter = require('matter-js');
+
+// Create.js => http://www.createjs.com
+// window.Create = require('createjs-broserify');
+
+
+window.Create = require('p5');
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,109 +3,104 @@
  * Custom scripts
  */
 
+
 window.onload = function(){
 
-(function (window, document, undefined) {
-    let wHeight = window.innerHeight;
-    let wWidth = window.innerWidth;
+        let wHeight = window.innerHeight;
+        let wWidth  = window.innerWidth;
 
-  var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    MouseConstraint = Matter.MouseConstraint,
-    Mouse = Matter.Mouse,
-    Bodies = Matter.Bodies;
-     var balls = [];
-    var engine = Engine.create();
-     var world = engine.world;
-    var render = Render.create({
-                element: document.body,
-                engine: engine,
-                options: {
-                    width: wWidth,
-                    height: wHeight,
-                    wireframes: false
-                }
-             });
+        let Engine          = Matter.Engine,
+            Render          = Matter.Render,
+            World           = Matter.World,
+            MouseConstraint = Matter.MouseConstraint,
+            Mouse           = Matter.Mouse,
+            Bodies          = Matter.Bodies,
+            engine          = Engine.create(),
+            world           = engine.world,
+            render          = Render.create({
+                                element: document.body,
+                                engine: engine,
+                                options: {
+                                    width: wWidth,
+                                    height: wHeight,
+                                    wireframes: false
+                                }
+                             });
 
-    /**
-     * Generate ramdom colors for the Charts (not to be use in production)
-     * @param {array} arr 
-     * @param {string} item
-     */
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF'.split('');
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+        /**
+         * Generate ramdom colors for the balls background
+         */
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
         }
-        return color;
-    }
-              
-    function myBall() {
-        this.ball = Bodies.circle(460, 10, 40, {
-              density: 0.001,
-              friction: 0.01,
-              frictionAir: 0.00001,
-              restitution: 0,
-              render: {
-                fillStyle: getRandomColor(),
-                strokeStyle: 'transparent',
-                lineWidth: 20
-              }
-        });
-        World.add(world, this.ball);
-    }
 
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
+        /**
+         * Balls Constructor
+         */ 
+        function createBall(x, y, r) {
+            let options = {
+                density: 0.00001,
+                friction: 0,
+                frictionAir: 0.00001,
+                restitution: 0.6,
                 render: {
-                    visible: false
+                    fillStyle: getRandomColor(),
+                    strokeStyle: 'transparent',
+                    lineWidth: 30
                 }
             }
-        });
 
-    World.add(engine.world, mouseConstraint);
+            this.ball = Bodies.circle( x, y, r, options );
+            World.add(world, this.ball);
+        }
 
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
+        /**
+         * Mouse control
+         */ 
+        var mouse = Mouse.create(render.canvas),
+            mouseConstraint = MouseConstraint.create(engine, {
+                mouse: mouse,
+                constraint: {
+                    stiffness: 0.2,
+                    render: {
+                        visible: false
+                    }
+                }
+            });
 
-    var ground = Bodies.rectangle(960, wHeight - 10, wWidth, 10, { isStatic: true });
-    var left = Bodies.rectangle(0, 500, 10, wHeight, { isStatic: true });
-    var right = Bodies.rectangle(wWidth, 500, 10, wHeight, { isStatic: true });
+        World.add(engine.world, mouseConstraint);
 
-    World.add(engine.world, [ground, left, right]);
+        // keep the mouse in sync with rendering
+        render.mouse = mouse;
 
-    function draw(){
-        new myBall();
-        // Engine.update(engine);
-    }    
+        // Creating bodies
+        var ground = Bodies.rectangle(wWidth/2, wHeight - 10, wWidth + 400, 10, { isStatic: true });
+        var leftWall = Bodies.rectangle(0, 500, 10, wHeight, { isStatic: true });
+        var rightWall = Bodies.rectangle(wWidth, 500, 10, wHeight, { isStatic: true });
 
-    var drawInterval =  window.setInterval(draw, 50);
-    setTimeout(function( ) { clearInterval( drawInterval ); }, 20000);
- 
-// World.add(engine.world, [boxA, ballA, ballB, ground]);
- 
-Engine.run(engine);
-Render.run(render);
+        World.add(engine.world, [ground, leftWall, rightWall]);
 
-})(window, document);
+        function draw(){
+            new createBall(460, 10, 40);
+        }    
 
+        var drawInterval = window.setInterval(draw, 50);
+        setTimeout( function() { clearInterval( drawInterval ); }, 20000 );
+     
+        Engine.run(engine);
+        Render.run(render);
+
+
+        // Matter.Events.on(mouseConstraint, "mousedown", function(){
+        //     console.log("whoop");
+        // })
+        // function mousePressed() {
+        //   new createBall(460, 10, 40);
+        // }
 
 }
-
-
-
-// $(function () {
-
-// alert(document.window);
-//     // let xxx = _.chunk(['a', 'b', 'c', 'd'], 2);
-
-//     // console.log(xxx);
-
-    
-
-// });
